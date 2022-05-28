f6421a2f464f40a293b270facbe4b67b5cb63b71
Kaden Hansen <kadendeanhansen@gmail.com>
2022-05-28 11:47:48 -0400

    add user persistence and cookie session data

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -9,14 +9,26 @@ router.post('/', async (req, res) => {
         where: { email: req.body.email }
     })
 
-    // console.log(user)
-
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({
             message: 'Could not find a user with the provided username and password'
         })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
+    }
+})
+
+router.get('/profile', async(req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
     }
 })
 
